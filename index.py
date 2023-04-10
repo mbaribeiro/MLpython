8162f452be1f6bb2bbd57cf906b74858bd1add41
Marlon Ribeiro <marlonbrenoribeiro@outlook.com>
2023-04-10 06:19:27 -0300

    add plots and r-square

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
 import numpy as np
 from tensorflow import keras
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
+
 
 # Create the model
 modelTemp = keras.Sequential([
@@ -75,8 +78,59 @@ with open('./tube/inputs/input.csv', 'r') as f:
     # Save predicted outputs
     for i in range(len(predicted_inputs)):
         inTemp, inVel, wallTemp = predicted_inputs[i]
-        with open(f'./tube/predicteds/outTemp{inTemp}outVel{inVel}wallTemp{wallTemp}.csv', 'w') as out_file:
+        with open(f'./tube/predicteds/inTemp{inTemp}inVel{inVel}wallTemp{wallTemp}.csv', 'w') as out_file:
             out_file.write('i,j,k,temp,vel,count\n')
-            pd = '\n'.join([f"{a},{b},{c}" for a, b, c in zip(
+            predicteds = '\n'.join([f"{a},{b},{c}" for a, b, c in zip(
                 coordenates, predicted_outputsTemp[i], predicted_outputsVel[i])]).replace('[', '').replace(']', '')
-            out_file.write(pd)
+            out_file.write(predicteds)
+
+# Plot the predicted outputs and the real outputs
+for i in range(len(predicted_inputs)):
+    inTemp, inVel, wallTemp = predicted_inputs[i]
+    output_file = f'./tube/traningOutputs/inTemp{inTemp}inVel{inVel}wallTemp{wallTemp}.csv'
+    df = pd.read_csv(output_file, delimiter='\t')
+
+    # Temperature
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+
+    output_list = df.iloc[:, 3].values.tolist()
+
+    nonzero_indices = [i for i in range(len(output_list)) if output_list[i] != 0]
+    output_list_filt = [output_list[i] for i in nonzero_indices]
+    predicted_outputsTemp_filt = [predicted_outputsTemp[i][j] for j in nonzero_indices]
+
+    m, b = np.polyfit(output_list_filt, predicted_outputsTemp_filt, 1)
+    x = np.array(output_list_filt)
+    ax1.scatter(output_list_filt, predicted_outputsTemp_filt)
+    ax1.plot(x, m*x + b, color='red')
+
+    result = r2_score(output_list, predicted_outputsTemp[i])
+    ax1.text(0.1, 0.9, "r-squared = {:.3f}".format(result), transform=ax1.transAxes)
+
+    print("R² (Temperature) = " + str(result))
+    ax1.set_title('Temperature [K]')
+    ax1.set_ylabel('Predicted [K]')
+    ax1.set_xlabel('Real [K]')
+
+    # Velocity
+    output_list = df.iloc[:, 4].values.tolist()
+
+    nonzero_indices = [i for i in range(len(output_list)) if output_list[i] != 0]
+    output_list_filt = [output_list[i] for i in nonzero_indices]
+    predicted_outputsVel_filt = [predicted_outputsVel[i][j] for j in nonzero_indices]
+
+    m, b = np.polyfit(output_list_filt, predicted_outputsVel_filt, 1)
+    x = np.array(output_list_filt)
+    ax2.scatter(output_list_filt,predicted_outputsVel_filt)
+    ax2.plot(x, m*x + b, color='red')
+
+    result = r2_score(output_list, predicted_outputsVel[i])
+    ax2.text(0.1, 0.9, "r-squared = {:.3f}".format(result), transform=ax2.transAxes)
+
+    print("R² (Velocity) = " + str(result))
+    ax2.set_title('Velocity [m s^-1]')
+    ax2.set_ylabel('Predicted [m s^-1]')
+    ax2.set_xlabel('Real [m s^-1]')
+
+    plt.show()
+
