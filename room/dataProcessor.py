bd3e83edafddf7b00eaef637e7d6acb798847ad3
Marlon Ribeiro <marlonbrenoribeiro@outlook.com>
2023-04-12 00:03:59 -0300

    update gitignore

---
+++ room/dataProcessor.py
@@ -0,0 +1,114 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from Scripts import mediaTempVel as mtv
+from Scripts import data as dt
+
+# Define the room dimensions
+sala = [-1500, 1500, 1500, -3000, -750, 2250]
+
+# Define the number of cubes
+n_cubes = 1000000
+
+# Define the path to the results folder
+results_path = './room/results/'
+
+# Loop through all files in the results folder
+for filename in os.listdir(results_path):
+    if filename.endswith('.csv'):
+        # Read data from file, skipping the first 6 rows
+        filepath = os.path.join(results_path, filename)
+        data = pd.read_csv(filepath, skiprows=6, usecols=[1,2,3,4,5], header=None)
+
+        # Convert data to a numpy array
+        data = data.values
+
+        # Change the value of 4th column to Celsius
+        data[:, 3] = data[:, 3] #- 273.15
+
+        # Plot the data
+        plt.show()
+
+        # Media Temp Velocity
+        result = mtv.media_temp_vel(sala, n_cubes, data)
+
+        # Create a list to store the values of i, j, k, temp, vel and count
+        values = []
+
+        # Loop through the keys of i
+        for i in result.keys():
+            # Loop through the keys of j
+            for j in result[i].keys():
+                # Loop through the keys of k
+                for k in result[i][j].keys():
+                    # Add the values to the list
+                    values.append([i, j, k, result[i][j][k]['temp'],
+                                  result[i][j][k]['vel'], result[i][j][k]['count']])
+
+        # Create a DataFrame with the values
+        df = pd.DataFrame(values, columns=['i', 'j', 'k', 'temp', 'vel', 'count'])
+
+        # Convert the values of i, j and k to int, if necessary
+        df['i'] = df['i'].astype(int)
+        df['j'] = df['j'].astype(int)
+        df['k'] = df['k'].astype(int)
+
+        # Print the DataFrame
+        print(df)
+
+        # Save the DataFrame to a file
+        output_filename = os.path.splitext(filename)[0] + '.csv'
+        output_filepath = os.path.join('./room/outputs/', output_filename)
+        df.to_csv(output_filepath, sep='\t', index=False)
+
+# Define the path to the results folder
+results_path = './room/trainingResults/'
+
+# Loop through all files in the results folder
+for filename in os.listdir(results_path):
+    if filename.endswith('.csv'):
+        # Read data from file, skipping the first 6 rows
+        filepath = os.path.join(results_path, filename)
+        data = pd.read_csv(filepath, skiprows=6, usecols=[1,2,3,4,5], header=None)
+
+        # Convert data to a numpy array
+        data = data.values
+
+        # Change the value of 4th column to Celsius
+        data[:, 3] = data[:, 3] #- 273.15
+
+        # Plot the data
+        plt.show()
+
+        # Media Temp Velocity
+        result = mtv.media_temp_vel(sala, n_cubes, data)
+
+        # Create a list to store the values of i, j, k, temp, vel and count
+        values = []
+
+        # Loop through the keys of i
+        for i in result.keys():
+            # Loop through the keys of j
+            for j in result[i].keys():
+                # Loop through the keys of k
+                for k in result[i][j].keys():
+                    # Add the values to the list
+                    values.append([i, j, k, result[i][j][k]['temp'],
+                                  result[i][j][k]['vel'], result[i][j][k]['count']])
+
+        # Create a DataFrame with the values
+        df = pd.DataFrame(values, columns=['i', 'j', 'k', 'temp', 'vel', 'count'])
+
+        # Convert the values of i, j and k to int, if necessary
+        df['i'] = df['i'].astype(int)
+        df['j'] = df['j'].astype(int)
+        df['k'] = df['k'].astype(int)
+
+        # Print the DataFrame
+        print(df)
+
+        # Save the DataFrame to a file
+        output_filename = os.path.splitext(filename)[0] + '.csv'
+        output_filepath = os.path.join('./room/trainingOutputs/', output_filename)
+        df.to_csv(output_filepath, sep='\t', index=False)
---
+++ room/index.py
@@ -0,0 +1,158 @@
+import numpy as np
+from tensorflow import keras
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+
+# Create the model
+modelTemp = keras.Sequential([
+    keras.layers.Dense(64, input_shape=(2,), activation='relu'),
+    keras.layers.Dense(1024, activation='relu'),
+    keras.layers.Dense(1022208, activation='linear')
+])
+
+modelVel = keras.Sequential([
+    keras.layers.Dense(64, input_shape=(2,), activation='sigmoid'),
+    keras.layers.Dense(1024, activation='sigmoid'),
+    keras.layers.Dense(1022208, activation='linear')
+])
+
+class PrintEpochCallback(keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        print(f"Epoch {epoch+1}/{self.params['epochs']}")
+
+# Compile the model
+modelTemp.compile(loss='mean_squared_error')
+modelVel.compile(loss='mean_squared_error')
+
+epoch_callback = PrintEpochCallback()
+
+# Load input data
+with open('./room/inputs/inputs.csv', 'r') as file:
+    # Skip the header
+    file.readline()
+    # Read data lines
+    data = []
+    for line in file:
+        values = [float(x) for x in line.strip().split(',')]
+        data.append(values)
+    # Convert data to numpy array
+    data = np.array(data)
+    print("reading inputs.csv...")
+
+# Separate input and output data
+inputs = data[:, :3]
+
+# Load input data
+with open('./room/inputs/inputs.csv', 'r') as f:
+    # Skip the header
+    next(f)
+    data1 = []
+    data2 = []
+    for line in f:
+        # Read input values
+        inTemp, inVel = map(str, line.strip().split(','))
+
+        # Load output data
+        output_file = f'./room/outputs/T{inTemp}V{inVel}.csv'
+        print("reading " + output_file + "...")
+        df = pd.read_csv(output_file, delimiter='\t')
+        coordenates = df.iloc[:, 0:3].values.tolist()
+        data_line1 = df.iloc[:, 3].astype(str).tolist()
+        data_line2 = df.iloc[:, 4].astype(str).tolist()
+        data1 = data1 + [data_line1]
+        data2 = data2 + [data_line2]
+
+    # Separate input and output data
+    outputs1 = data1
+    outputs2 = data2
+
+    outputs1 = np.array(outputs1, dtype=float)
+    outputs2 = np.array(outputs2, dtype=float)
+
+    # Train the modelTemp
+    modelTemp.fit(inputs, outputs1, epochs=500, batch_size=32, verbose=0, callbacks=[epoch_callback])
+
+    # Train the modelVel
+    modelVel.fit(inputs, outputs2, epochs=500, batch_size=32, verbose=0, callbacks=[epoch_callback])
+
+    # Test the model
+    predicted_inputs = [[15, 1], [18, 5], [20, 3]]
+    test_inputs = np.array(predicted_inputs)
+    predicted_outputsTemp = modelTemp.predict(test_inputs)
+    predicted_outputsVel = modelVel.predict(test_inputs)
+
+    # Save predicted outputs
+    for i in range(len(predicted_inputs)):
+        inTemp, inVel = predicted_inputs[i]
+        with open(f'./room/predicteds/T{inTemp}V{inVel}.csv', 'w') as out_file:
+            out_file.write('i,j,k,temp,vel,count\n')
+            predicteds = '\n'.join([f"{a},{b},{c}" for a, b, c in zip(
+                coordenates, predicted_outputsTemp[i], predicted_outputsVel[i])]).replace('[', '').replace(']', '')
+            out_file.write(predicteds)
+
+# Plot the predicted outputs and the real outputs
+for i in range(len(predicted_inputs)):
+    inTemp, inVel = predicted_inputs[i]
+    output_file = f'./room/trainingOutputs/T{inTemp}V{inVel}.csv'
+    df = pd.read_csv(output_file, delimiter='\t')
+
+    # Temperature
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+
+    output_list = df.iloc[:, 3].values.tolist()
+
+    nonzero_indices = [i for i in range(
+        len(output_list)) if output_list[i] != 0]
+    output_list_filt = [output_list[i] for i in nonzero_indices]
+    predicted_outputsTemp_filt = [
+        predicted_outputsTemp[i][j] for j in nonzero_indices]
+
+    m, b = np.polyfit(output_list_filt, predicted_outputsTemp_filt, 1)
+    x = np.array(output_list_filt)
+    ax1.scatter(output_list_filt, predicted_outputsTemp_filt)
+    ax1.plot(x, m*x + b, color='red')
+
+    result = r2_score(output_list, predicted_outputsTemp[i])
+    ax1.text(
+        0.1, 0.9, "r-squared = {:.3f}".format(result), transform=ax1.transAxes)
+
+    print("R² (Temperature) = " + str(result))
+    ax1.set_title('Temperature [K]')
+    ax1.set_ylabel('Predicted [K]')
+    ax1.set_xlabel('Real [K]')
+
+    #calculate the mse
+    mse = keras.losses.mean_squared_error(output_list, predicted_outputsTemp[i])
+    print("MSE (Temperature) = " + str(mse))
+
+    # Velocity
+    output_list = df.iloc[:, 4].values.tolist()
+
+    nonzero_indices = [i for i in range(
+        len(output_list)) if output_list[i] != 0]
+    output_list_filt = [output_list[i] for i in nonzero_indices]
+    predicted_outputsVel_filt = [
+        predicted_outputsVel[i][j] for j in nonzero_indices]
+
+    m, b = np.polyfit(output_list_filt, predicted_outputsVel_filt, 1)
+    x = np.array(output_list_filt)
+    ax2.scatter(output_list_filt, predicted_outputsVel_filt)
+    ax2.plot(x, m*x + b, color='red')
+
+    result = r2_score(output_list, predicted_outputsVel[i])
+    ax2.text(
+        0.1, 0.9, "r-squared = {:.3f}".format(result), transform=ax2.transAxes)
+
+    print("R² (Velocity) = " + str(result))
+    ax2.set_title('Velocity [m s^-1]')
+    ax2.set_ylabel('Predicted [m s^-1]')
+    ax2.set_xlabel('Real [m s^-1]')
+
+    #calculate the mse
+    mse = keras.losses.mean_squared_error(output_list, predicted_outputsVel[i])
+    print("MSE (Velocity) = " + str(mse))
+
+
+    plt.show()
